64f527fa1fbf1035d6dfce063cdab56cbb8aa13d
sondmfpt <sondmhe186291@fpt.edu.vn>
2025-09-13 00:15:36 +0700

    update group form: search user

--- src/screens/GroupFormScreen.tsx
+++ src/screens/GroupFormScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-    import { SafeAreaView, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import type { RouteProp } from "@react-navigation/native";
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
@@ -7,7 +7,9 @@ import { RootStackParamList } from "../../App";
 import AppInput from "../components/AppInput";
 import AppButton from "../components/AppButton";
 import { Group } from "../store/slices/groupsSlice";
- 
+import { User } from "../store/slices/userSlice";
+import { getUserByEmail } from "../store/slices/userSlice";
+
 type GroupFormRouteProp = RouteProp<RootStackParamList, "GroupForm">;
 
 export default function GroupFormScreen() {
@@ -17,6 +19,42 @@ export default function GroupFormScreen() {
   const [formData, setFormData] = useState<Group>(
     groupData ?? { id: "", name: "", users: [], createdAt: new Date().toISOString() }
   );
+  const [isOpenDropDown, setIsOpenDropDown] = useState(false);
+  const [searchUser, setSearchUser] = useState<User>();
+  const [searchEmail, setSearchEmail] = useState<string>('');
+
+  function checkEmailSuffix(text: string) {
+    const regex = /@(gmail\.com|yahoo\.com|outlook\.com)$/;
+    return regex.test(text);
+  }
+  const handleSearchUser = async (e: string) => {
+    const email = e.toLowerCase();
+    setSearchEmail(email);
+
+    if (email && checkEmailSuffix(email)) {
+      const user = await getUserByEmail({ email });
+      if (user) {
+        setSearchUser(user);
+        setIsOpenDropDown(true);
+      } else {
+        setIsOpenDropDown(false);
+      }
+    } else {
+      setIsOpenDropDown(false);
+    }
+
+  };
+
+  const handleChooseUser = () => {
+    if (searchUser) {
+      setIsOpenDropDown(false);
+      setSearchEmail('');
+      if (formData.users?.find((user) => user.id === searchUser.id)) {
+        return;
+      }
+      setFormData({ ...formData, users: [...formData.users || [], searchUser] });
+    }
+  }
 
   function handleCreate() {
     navigation.goBack();
@@ -25,32 +63,62 @@ export default function GroupFormScreen() {
   return (
     <SafeAreaView className="flex-1 p-4">
       <Text className="text-lg font-bold mb-4">Tạo nhóm mới</Text>
+      <View className="mb-4">
+        <Text className="text-sm text-slate-600 mb-2">Tên nhóm</Text>
+        <AppInput
+          value={formData.name}
+          onChangeText={(text) => setFormData({ ...formData, name: text })}
+          placeholder="Ví dụ: Roommates"
+        />
+      </View>
 
-      <Text className="text-sm text-slate-600 mb-2">Tên nhóm</Text>
-      <AppInput
-        value={formData.name}
-        onChangeText={(text) => setFormData({ ...formData, name: text })}
-        placeholder="Ví dụ: Roommates"
-      />
 
+      <View className="mb-4">
+        <Text className="text-sm text-slate-600 mb-2">Số lượng người</Text>
+        <AppInput
+          value={formData.users?.length?.toString() || '0'}
+          editable={false}
+        />
+      </View>
 
-      <Text className="text-sm text-slate-600 mb-2">Số lượng người</Text>
-      <AppInput
-        value={formData.users?.length?.toString() || '0'}
-        editable={false}
-      />
+      <View className="mb-4">
+        <Text className="text-sm text-slate-600 mb-2">Thành viên</Text>
+        <View className="flex flex-col border border-slate-300 rounded-lg px-2 gap-2">
+          <View className="flex-row gap-2 p-2 overflow-x-auto">
+            {formData.users?.map((user) => (
+              <Text key={user.id} className="bg-blue-500 text-white rounded-full py-1 px-4 active:bg-blue-600">{user.name}</Text>
+            ))}
+          </View>
+          <AppInput
+            placeholder="Tìm kiếm..."
+            onChangeText={(text) => handleSearchUser(text)}
+            value={searchEmail}
+          />
+          {isOpenDropDown && (
+            <TouchableOpacity
+              onPress={handleChooseUser}
+              className="border border-slate-300 rounded-lg p-2 mb-2">
+              <View className="flex flex-row gap-2 w-full">
+                <View className="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center">
+                  <Text className="text-white text-center">{searchUser?.name.split(' ')[0].slice(0, 1).toUpperCase()}</Text>
+                </View>
+                <View className="flex-1 flex flex-col gap-1">
+                  <Text className="text-sm font-bold">{searchUser?.name}</Text>
+                  <Text className="text-xs text-slate-500">{searchUser?.email}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
 
-      <Text className="text-sm text-slate-600 mb-2">Thành viên</Text>
-      <AppInput
-        value={formData.users?.map((user) => user.name).join(', ') || ''}
-        placeholder="Ví dụ: 3"
-      />
-
-      <Text className="text-sm text-slate-600 mb-2">Ngày tạo</Text>
-      <AppInput
-        value={formData.createdAt ? new Date(formData.createdAt).toLocaleDateString("vi-VN") : new Date().toLocaleDateString("vi-VN")}
-        editable={false}
-      />
+      <View className="mb-4">
+        <Text className="text-sm text-slate-600 mb-2">Ngày tạo</Text>
+        <AppInput
+          value={formData.createdAt ? new Date(formData.createdAt).toLocaleDateString("vi-VN") : new Date().toLocaleDateString("vi-VN")}
+          editable={false}
+        />
+      </View>
       <AppButton
         title="Tạo nhóm"
         variant="primary"
